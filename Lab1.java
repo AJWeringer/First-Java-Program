a151d87efbff85dcf2f5675b89cf600049875d42
Anthony Weringer <34197089+AJWeringer@users.noreply.github.com>
2018-11-02 13:01:49 -0700

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,15 @@
+//***********************************************************
+// Name:  Anthony Weringer
+// Title: Lab1.java
+// Description: Prints out My first program and my name
+// Time spent: 20 minutes
+// Date: 8/17/17
+//***********************************************************
+public class Lab1 
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Hello my name is Anthony Weringer.");
+		System.out.println("This is my first program!");
+	}
+}
